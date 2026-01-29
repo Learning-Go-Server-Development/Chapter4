0967332445d3c8cf18fac644bf88f2b38781517f
KenWilliamson <kenulborapublic@gmail.com>
2026-01-29 18:57:00 -0500

    update

--- proxy.go
+++ proxy.go
@@ -5,7 +5,6 @@ import "net/http"
 // Proxy Proxy
 type Proxy interface {
 	Do(req *http.Request, obj any) (bool, int)
-	//DoNonJSON(req *http.Request) (bool, int, []byte)
 }
 
 // go mod init github.com/Learning-Go-Server-Development/Chapter4
